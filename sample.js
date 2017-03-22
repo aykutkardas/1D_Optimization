583b969ce203d8d1337a0d444615941c38e4611f
Aykut Kardaş <aykutkrds@gmail.com>
2017-03-22 15:44:24 +0300

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,26 +1,12 @@
-var ITEMS = [{
-    len: 1966,
-    q: 8
-  }, {
-    len: 400,
-    q: 12
-  }, {
-    len: 300,
-    q: 4
-  }, {
-    len: 1276,
-    q: 4
-  }, {
-    len: 805,
-    q: 6
-  }, {
-    len: 127,
-    q: 6
-  }];
+/*
+ * @Name:   RocketCut v1.0.0 
+ * @Desc:   1D Optimization Tools
+ * @Author: Aykut Kardaş
+ *
+ */
 
 
 function REPORT(RESULT) {
-  "use strict";
 
   var i, j,
     REP = [],
@@ -41,10 +27,28 @@ function REPORT(RESULT) {
 
     } else {
 
-      //console.log(index);
+      var tmpr = tmp_result.split(",").slice(1, 1000);
+      var blocks = [];
+      var bblocks = [];
+
+      for (z = 0; z < tmpr.length; z++) {
+        var indx = blocks.indexOf(tmpr[z]);
+        if (indx > -1) {
+          bblocks[indx].q += 1;
+        } else {
+          blocks.push(tmpr[z]);
+          bblocks.push({
+            list: tmpr[z],
+            q: 1
+          })
+        }
+
+      }
+
       REP.push(tmp_result);
       BREP.push({
         list: tmp_result.split(",").slice(1, 1000),
+        block: bblocks,
         q: 1
       });
 
@@ -52,16 +56,10 @@ function REPORT(RESULT) {
 
   }
 
-  for (j = 1; j < BREP.length; j += 1) {
-    console.log("Kesim Şekli: " + BREP[j].list + "\nKesim Adedi: " + BREP[j].q);
-  }
-  console.timeEnd("b");
+  return [BREP.slice(1, BREP.length)];
 }
 
 function OPT(ITEMS, LIMIT) {
-  console.time("a");
-
-  "use strict";
 
   // KULLANILACAK DEĞİŞKENLERİ OLUŞTUR
   var NEW_ITEMS = [9e+9],
@@ -86,35 +84,21 @@ function OPT(ITEMS, LIMIT) {
 
   // SAYIYI BUYUKTEN KUCUGE SIRALAMA FONKSIYONU
   function nSort(a, b) {
-
-    if (a > b) {
-
-      return -1;
-
-    } else if (a < b) {
-
-      return 1;
-
-    } else {
-
-      return 0;
-
-    }
-
+    if (a > b) return -1;
+    else if (a < b) return 1;
+    else return 0;
   }
 
   // YENI ITEM LISTESINI BUYUKTEN KUCUGE SIRALA
   NEW_ITEMS = NEW_ITEMS.sort(nSort);
 
-  // YENI ITEM LISTESINI EKRANA BAS
-  //console.log(NEW_ITEMS);
-
 
   // TÜM ÖLÇÜLERDE DÖNGÜYE GİR
   for (z = 0; z <= NEW_ITEMS.length; z += 1) {
 
-    // EĞER PARÇA EKLENDİKTEN SONRA LIMIT TE KALAN ALAN -1 DEN BÜYÜKSE // VE SIRADAKİ PARÇA KALAN ALANDAN KÜÇÜK YA DA EŞİTSE
-    if ((LIMIT - TEMP[0]) > 0 && NEW_ITEMS[z] <= (LIMIT - TEMP[0])) {
+    // EĞER PARÇA EKLENDİKTEN SONRA LIMIT TE KALAN ALAN -1 DEN BÜYÜKSE 
+    // VE SIRADAKİ PARÇA KALAN ALANDAN KÜÇÜK YA DA EŞİTSE
+    if ((LIMIT - TEMP[0]) > -1 && NEW_ITEMS[z] <= (LIMIT - TEMP[0])) {
 
       // PARÇA KALAN ALANINI PARCA EKLEDİKTEN SONRAKİ HALİNE GÜNCELLE.
       TEMP[0] = TEMP[0] + NEW_ITEMS[z] + 5;
@@ -125,7 +109,6 @@ function OPT(ITEMS, LIMIT) {
       // ANA LİSTEDEN PARÇAYI ÇIKAR.
       NEW_ITEMS.splice(z, 1);
 
-      // EKLENEN PARÇALAR LİSTESİNİ EKRANA BAS
       z = 0;
 
       // NAKARAT #
@@ -143,7 +126,6 @@ function OPT(ITEMS, LIMIT) {
           // ANA LİSTEDEN PARÇAYI ÇIKAR.
           NEW_ITEMS.splice(y, 1);
 
-          // EKLENEN PARÇALAR LİSTESİNİ EKRANA BAS
           y = 0;
 
         }
@@ -153,8 +135,6 @@ function OPT(ITEMS, LIMIT) {
       // EĞER PARÇA EKLENDİKTEN SONRA KALAN ALAN 0 DAN KÜÇÜKSE
     } else {
 
-      // EKLENEN PARÇALAR LSİTESİNİ EKRANA BAS
-      //console.log(TEMP);
 
       // EKLENEN PARÇALAR LİSTESİNİ, SONUCA EKLE.
       RESULT.push(TEMP);
@@ -167,5 +147,5 @@ function OPT(ITEMS, LIMIT) {
   }
 
   // SONUCU EKRANA BAS
-  REPORT(RESULT);
+  return REPORT(RESULT);
 }
